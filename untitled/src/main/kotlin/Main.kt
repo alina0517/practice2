78ee2ccf6c75c3ab2e43519f0409e3f1356a19fa
alina0517 <144257716+alina0517@users.noreply.github.com>
2023-09-07 17:28:49 +0300

    Add files via upload

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main(args: Array<String>) {
+    //1
+    val age1=42
+    val age2=21
+    //
+    //2
+    val avg1=(age1+age2)/2
+    //
+    //3
+    /*
+    val age1=42.0;
+    val age2=21.0;
+    val avg1=(age1+age2)/2;
+    */
+    //
+    //4
+    val firstName:String="Алина";
+    val lastName:String="Махлай";
+    //
+    //5
+    val fullName:String=firstName+lastName;
+    //
+    //6
+    val myDetails:String="Привет, меня зовут"+fullName;
+    println(myDetails)
+    //
+    //7
+    val test: Triple <Int, Int, Int> = Triple (7,9,23)
+    //8
+    val month = test.first
+    val day = test.second
+    val year= test.third
+   //9
+
+}
